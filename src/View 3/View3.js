c9e42980da616be861c8c61b5281b2692b70d895
mahmudh <mahmud.hussain@digital.cabinet-office.gov.uk>
2018-04-05 16:52:55 +0100

    Add additional message

--- src/View 3/View3.js
+++ src/View 3/View3.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import './View3.css';
 import Logo from '../_components/logo/logo';
 import Clock from '../_components/clock/clock';
-import PrayerTimes from '../_components/prayer-times/prayer-times';
 import Date from '../_components/date/date';
 import BuilderNumber from '../_components/build-number/build-number';
 import NextJamahTime from '../_components/next-jamah-time/next-jamah-time';
+import AdditionalMessage from '../_components/additional-message/additional-message';
 
 class View3 extends Component {
   render() {
@@ -22,12 +22,17 @@ class View3 extends Component {
             <div className="row">
               <Date />
             </div>
-            <h1>View 3</h1>
+            <div className="row">
+              <AdditionalMessage
+                message="
+                                    <p><i>Please ensure your</i></p>
+                                    <p><i>mobile phone is silent</i></p>
+                                    <p><i>in the prayer hall</i></p>
+                                "
+              />
+            </div>
           </div>
           <div className="col-12 col-md-6">
-            {/*<div className="row">*/}
-            {/*<NextJamahTime />*/}
-            {/*</div>*/}
             <div className="row">
               <NextJamahTime />
             </div>
