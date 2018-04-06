889da8cefb1ac1bf58152ded2b47b89bbf0baa1b
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2018-04-06 15:36:00 +0100

    updates

--- src/_components/prayer-times-week-ahead/prayer-times-week-ahead.js
+++ src/_components/prayer-times-week-ahead/prayer-times-week-ahead.js
@@ -11,56 +11,66 @@ class PrayerTimesWeekAhead extends Component {
     };
   }
 
-  getPrayerTimes() {
-    var date = moment().format('DD/MM/YYYY');
+  getPrayerTimes(additional_days = 0) {
+    var date = moment()
+      .add(additional_days, 'days')
+      .format('DD/MM/YYYY');
     return data[date];
   }
 
-  render() {
+  getNext7daysTableRows() {
     var rows = [];
     var numrows = 7;
-    for (var i = 0; i < numrows; i++) {
+
+    for (var i = 1; i <= numrows; i++) {
       // note: we add a key prop here to allow react to uniquely identify each
       // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
 
+      var times = this.getPrayerTimes(i);
       rows.push(
-        <tr>
-          <td>Date</td>
+        <tr key={i}>
+          <td>{times['Date']}</td>
           {/* FAJR */}
-          <td>Begins</td>
-          <td>Jama'ah</td>
-          <td>Sunrise</td>
+          <td>{times['Fajr Begins']}</td>
+          <td>{times['Fajr Jama‘ah']}</td>
+          <td>{times['Sunrise']}</td>
 
           {/* ZUHR */}
-          <td>Begins</td>
-          <td>Jama'ah</td>
+          <td>{times['Zuhr Begins']}</td>
+          <td>{times['Zuhr Jama‘ah']}</td>
 
           {/* ASR */}
-          <td>Mithl 1</td>
-          <td>Mithl 2</td>
-          <td>Jama'ah</td>
+          <td>{times['Asr Mithl 1']}</td>
+          <td>{times['Asr Mithl 2']}</td>
+          <td>{times['Asr Jama‘ah']}</td>
 
           {/* MAGHRIB */}
-          <td>Begins</td>
-          <td>Jama'ah</td>
+          <td>{times['Maghrib Begins']}</td>
+          <td>{times['Maghrib Jama‘ah']}</td>
 
           {/* ISHA */}
-          <td>Begins</td>
-          <td>Jama'ah</td>
+          <td>{times['Isha Begins']}</td>
+          <td>{times['Isha Jama‘ah']}</td>
         </tr>
       );
     }
+
+    return rows;
+  }
+
+  render() {
+    var rows = this.getNext7daysTableRows();
     return (
       <div className="PrayerTimesWeekAheadWrapper">
         <table className="PrayerTimesWeekAhead">
           <thead>
             <tr>
               <th>Week ahead</th>
-              <th colspan="3">Fajr</th>
-              <th colspan="2">Zuhr</th>
-              <th colspan="3">Asr</th>
-              <th colspan="2">Maghrib</th>
-              <th colspan="2">Isha</th>
+              <th colSpan="3">Fajr</th>
+              <th colSpan="2">Zuhr</th>
+              <th colSpan="3">Asr</th>
+              <th colSpan="2">Maghrib</th>
+              <th colSpan="2">Isha</th>
             </tr>
           </thead>
           <tbody>
