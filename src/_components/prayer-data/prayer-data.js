dc190d1cbcf4d1add0ff82508e2c26924c8b2f5e
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2019-01-19 01:58:07 +0000

    now updating from spreadsheet

--- src/_components/prayer-data/prayer-data.js
+++ src/_components/prayer-data/prayer-data.js
@@ -1,10 +1,58 @@
 import moment from 'moment';
-import data from '../../_assets/data/elm-prayer-times-2019.json';
+import config from '../../config.json';
+import csvtojson from 'csvtojson';
+import request from 'request';
 
 class PrayerData {
+  constructor() {
+    this.updateData();
+  }
+
   getPrayerTimes(date = null) {
     date = date === null ? moment().format('DD/MM/YYYY') : date;
-    return data[date];
+    var data = this.getPrayerData();
+    return data ? data[date] : [];
+  }
+
+  getPrayerTimesFromGoogleSheets() {
+    if (!config.googleSheets.prayerData) {
+      alert('CSV not set');
+    }
+
+    return csvtojson()
+      .fromStream(request.get(config.googleSheets.prayerData))
+      .then(json => {
+        this.storePrayerData(json);
+      });
+  }
+
+  storePrayerData(prayerData = []) {
+    var formatted_data = {};
+    prayerData.forEach(day => {
+      formatted_data[day.Date] = day;
+    });
+    window.localStorage.setItem('prayerData', JSON.stringify(formatted_data));
+    window.localStorage.setItem('lastUpdated', moment().unix());
+  }
+
+  getPrayerData() {
+    var _prayerData = window.localStorage.getItem('prayerData');
+    return _prayerData ? JSON.parse(_prayerData) : null;
+  }
+
+  getLastUpdatedTime() {
+    return window.localStorage.getItem('lastUpdated');
+  }
+
+  updateData() {
+    var lastUpdatedDiff = moment().unix() - parseInt(this.getLastUpdatedTime());
+    if (
+      lastUpdatedDiff > config.googleSheets.refreshRate * 60 ||
+      !this.getPrayerData()
+    ) {
+      this.getPrayerTimesFromGoogleSheets();
+      console.info('Updating Data....');
+    }
   }
 }
 
