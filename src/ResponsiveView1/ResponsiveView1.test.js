b6c491f8049514d0ef7f41c01abd843033e06f03
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2019-02-09 19:59:23 +0000

    remove unused view

--- src/ResponsiveView1/ResponsiveView1.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ResponsiveView1 from './ResponsiveView1';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ResponsiveView1 />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
