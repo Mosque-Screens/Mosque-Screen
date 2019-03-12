f8faae6b4dd1d69f0ab34c9406be09430975ea04
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2019-03-12 20:00:14 +0000

    fixes typos

--- src/_components/next-jamah-time/next-jamah-time.test.js
+++ src/_components/next-jamah-time/next-jamah-time.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NextJamahTime from './next-jamah-time';
+import NextJammahTime from './next-jamah-time';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<NextJamahTime />, div);
+  ReactDOM.render(<NextJammahTime />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
