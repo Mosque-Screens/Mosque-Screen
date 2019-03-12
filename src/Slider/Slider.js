abdda42e8ed1bc9ba3cc90dc0deb63c7c0904913
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2019-03-12 03:44:40 +0000

    Adds single view mode

--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -5,6 +5,7 @@ import View2 from '../Views/View 2/View2';
 import View3 from '../Views/View 3/View3';
 import View4 from '../Views/View 4/View4';
 import View6 from '../Views/View 6/View6';
+import SingleView from '../Views/SingleView/SingleView';
 import GoogleSlides from '../GoogleSlides/GoogleSlides';
 import config from '../config.json';
 import BlackoutPeriods from '../_components/blackout-periods/blackout-periods';
@@ -15,10 +16,13 @@ class Slider extends Component {
     super(props);
     var _appConfig = new AppConfig();
     this.state = {
-      currentSlide: this.getInitialSlide(),
+      currentSlide:
+        _appConfig.get('sliderMode') === 'single-view'
+          ? this.getSingleView()
+          : this.getInitialSlide(),
       slides: this.getSlides(),
       currentPosition: 0,
-      sliderMode: config.sliderMode || 'slider',
+      sliderMode: _appConfig.get('sliderMode') || 'slider',
       slideTimeout: _appConfig.get('sliderTimeout') || 8000,
       googleSlides: {
         slide: <GoogleSlides />,
@@ -32,6 +36,10 @@ class Slider extends Component {
 
   getInitialSlide() {
     return <View1 />;
+  }
+
+  getSingleView() {
+    return <SingleView />;
   }
 
   getSlides() {
