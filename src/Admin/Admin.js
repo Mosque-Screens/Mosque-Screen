67170807d6a8fd7f68b11a1fb0657e1ed2410e0c
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2019-03-17 16:53:54 +0000

    Adds admin route and placeholder view for admin

---
+++ src/Admin/Admin.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Admin.css';
+
+class Admin extends Component {
+  render() {
+    return (
+      <div className="AdminWrapper">
+        <h1>Admin</h1>
+        <div className="AdminContent">
+          <h2>Admin interface coming soon.</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Admin;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './_assets/fonts/fonts.css';
 import './index.css';
 import Slider from './Slider/Slider';
+import Admin from './Admin/Admin';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap-4-grid/css/grid.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<Slider />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <Switch>
+      <Route path="/admin" exact component={Admin} />
+      <Route component={Slider} />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 registerServiceWorker();
