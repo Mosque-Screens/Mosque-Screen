ffa8338b483e22b73dcaad42d0bd0692be73a92f
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-03-19 14:18:52 +0000

    Now authenticated

--- src/_components/google-auth/google-auth.js
+++ src/_components/google-auth/google-auth.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import './google-auth.css';
-import gapi from 'gapi-client';
-
 class GoogleAuth extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      gapi: window.gapi,
       apiKey: 'AIzaSyDm5iZU5kvCRbJKeh5MrO5sDH0bBgXlpD4',
       discoveryDocs: [
         'https://people.googleapis.com/$discovery/rest?version=v1'
@@ -13,18 +12,16 @@ class GoogleAuth extends Component {
       clientId:
         '264729051003-tmkiqo3dsnn2q957mmro8e9ds5i5516r.apps.googleusercontent.com',
       scopes: 'profile',
-      authorizeButton: document.getElementById('authorize-button'),
-      signoutButton: document.getElementById('signout-button')
+      userName: null,
+      isSignedIn: false
     };
+
+    this.state.gapi.load('client:auth2', this.initClient);
   }
 
-  handleClientLoad() {
-    // Load the API client and auth2 library
-    gapi.load('client:auth2', this.initClient);
-  }
-
-  initClient() {
-    gapi.client
+  initClient = () => {
+    var _this = this;
+    this.state.gapi.client
       .init({
         apiKey: this.state.apiKey,
         discoveryDocs: this.state.discoveryDocs,
@@ -32,52 +29,66 @@ class GoogleAuth extends Component {
         scope: this.state.scopes
       })
       .then(function() {
-        // Listen for sign-in state changes.
-        gapi.auth2.getAuthInstance().isSignedIn.listen(this.updateSigninStatus);
-
-        // Handle the initial sign-in state.
-        this.updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-
-        this.state.authorizeButton.onclick = this.handleAuthClick;
-        this.state.signoutButton.onclick = this.handleSignoutClick;
+        _this.state.gapi.auth2
+          .getAuthInstance()
+          .isSignedIn.listen(_this.updateSigninStatus);
+        _this.updateSigninStatus(
+          _this.state.gapi.auth2.getAuthInstance().isSignedIn.get()
+        );
       });
-  }
+  };
 
   updateSigninStatus(isSignedIn) {
-    if (isSignedIn) {
-      this.state.authorizeButton.style.display = 'none';
-      this.state.signoutButton.style.display = 'block';
+    if (this && isSignedIn) {
       this.makeApiCall();
-    } else {
-      this.state.authorizeButton.style.display = 'block';
-      this.state.signoutButton.style.display = 'none';
+      this.setState(() => ({
+        isSignedIn: isSignedIn
+      }));
+    } else if (!this) {
+      window.location.reload();
     }
   }
 
   handleAuthClick() {
-    gapi.auth2.getAuthInstance().signIn();
+    window.gapi.auth2.getAuthInstance().signIn();
   }
 
   handleSignoutClick() {
-    gapi.auth2.getAuthInstance().signOut();
+    window.gapi.auth2.getAuthInstance().signOut();
   }
 
   makeApiCall() {
-    gapi.client.people.people
+    var _this = this;
+    this.state.gapi.client.people.people
       .get({
         resourceName: 'people/me',
         'requestMask.includeField': 'person.names'
       })
       .then(function(resp) {
-        var p = document.createElement('p');
         var name = resp.result.names[0].givenName;
-        p.appendChild(document.createTextNode('Hello, ' + name + '!'));
-        document.getElementById('content').appendChild(p);
+        _this.setState(() => ({
+          userName: name
+        }));
       });
   }
 
   render() {
-    return <div className="GoogleAuthWrapper" />;
+    var AuthenticatedMessage = null;
+
+    if (this.state.isSignedIn) {
+      AuthenticatedMessage = (
+        <div className="AuthenticatedMessage">
+          <p>Hi {this.state.userName}.</p>
+          <button onClick={this.handleSignoutClick}>Sign out</button>
+        </div>
+      );
+    } else {
+      AuthenticatedMessage = (
+        <button onClick={this.handleAuthClick}>Sign in with Google</button>
+      );
+    }
+
+    return <div className="GoogleAuthWrapper">{AuthenticatedMessage}</div>;
   }
 }
 
