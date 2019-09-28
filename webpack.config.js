227f837a4bc8d9f0a77c6598048a784fc49fbbba
Maslax Ali <alimaslax@gmail.com>
2019-09-28 00:08:41 -0400

    Webpack and Babel added to bundle application

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new
+    HTMLWebpackPlugin({
+        template: __dirname + '/public/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    });
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            { 
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit:100000
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    node: {
+        fs: 'empty',
+        tls: 'empty',
+        net: 'empty'
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [HTMLWebpackPluginConfig]
+};
